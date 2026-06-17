9a72e312e340b387578030ace39bcb51165a4664
PP L <Suc2es2>
2026-06-18 02:28:39 +0800

    Site updated: 2026-06-18 02:28:32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1781662678943";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1781720910515";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
