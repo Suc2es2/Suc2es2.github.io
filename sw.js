b56b955fcffccc9246258fcc4a7c4022b2936836
Suc2es2 <suc2es2.clc@gmail.com>
2026-08-26 22:32:46 +0800

    Site updated: 2026-08-26 22:32:34

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1787463250337";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1787754749556";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
